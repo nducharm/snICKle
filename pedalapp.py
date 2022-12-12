f143e4f60aa1a556d2dd519c241a6ce8c7972a8c
nptducharme <nptducharme@gmail.com>
2022-12-12 01:20:40 -0800

    .gitignore virtual environment and added prototype GUI code in pedalapp

---
+++ pedalapp.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class GUI:      # we do everything inside a class to make it easier for GUI elements and functions to interact
+
+    def __init__(self) -> None:         # initiator contains the main GUI code
+        self.root = tk.Tk()                                     # instantiate the Tk class. root is the standard name for this object
+
+        self.root.geometry("1000x800")                          # define default window size and title
+        self.root.title("The Dogshit Digital Amplifier")
+
+        self.root.mainloop()                                    # render the root Tk window
+
+GUI()
